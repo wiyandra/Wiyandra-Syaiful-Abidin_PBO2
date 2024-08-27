125fb082dde95659d344d216231dcfcb23b5c399
Wiyandra Syaiful Abidin <wiyandrasfl@students.untidar.ac.id>
2024-08-27 09:12:57 +0700

    proyek1 commited

---
+++ proyek1/KodeProgramTugasPBO2_2310506009.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wiyandraproject1;
+
+/**
+ *
+ * @author Wiyandra
+ */
+public class Wiyandraproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("NAMA : Wiyandra Syaiful Abidin");
+        System.out.println("NPM : 2310506009");
+        System.out.println("ALAMAT : Bekasi Barat");
+        System.out.println("NO_HP : 087875142643");
+    }
+}
